59c096aa45511efc27c8d1924ccd13766e51a982
Fernando Cruz <cruzfernandoh@gmail.com>
2023-09-07 19:11:23 -0300

    init project and add model

---
+++ src/main/java/com/fernandoh/model/Book.java
@@ -0,0 +1,73 @@
+package com.fernandoh.model;
+
+import com.fernandoh.dto.BookDTO;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String author;
+    private BigDecimal price;
+
+    public Book(Book book) {
+        this.id = book.getId();
+        this.name = book.getName();
+        this.author = book.getAuthor();
+        this.price = book.getPrice();
+    }
+
+    public Book(BookDTO bookDTO) {
+        this.name = bookDTO.name();
+        this.author = bookDTO.author();
+        this.price = bookDTO.price();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Books{" +
+                "Id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/com/fernandoh/model/Client.java
@@ -0,0 +1,108 @@
+package com.fernandoh.model;
+
+import com.fernandoh.dto.ClientDTO;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String document;
+    private BigDecimal balance;
+    @ManyToMany
+    private List<Book> books;
+
+    public Client(){}
+
+    public Client(Client client) {
+        this.id = client.getId();
+        this.firstName = client.getFirstName();
+        this.lastName = client.getLastName();
+        this.document = client.getDocument();
+        this.balance = client.getBalance();
+        this.books = client.getBooks();
+    }
+
+    public Client(ClientDTO clientDTO) {
+        this.firstName = clientDTO.firstName();
+        this.lastName = clientDTO.lastName();
+        this.document = clientDTO.document();
+        this.balance = clientDTO.balance();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public void deposit(BigDecimal value) {
+        this.balance = this.balance.add(value);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", document='" + document + '\'' +
+                ", balance=" + balance +
+                ", books=" + books +
+                '}';
+    }
+}
