04d7ecf3bb6df0f4e1f6f97dcb287bb1a7033f79
Fernando Cruz <cruzfernandoh@gmail.com>
2023-09-07 19:11:58 -0300

    create repositories

---
+++ src/main/java/com/fernandoh/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.fernandoh.repository;
+
+import com.fernandoh.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
